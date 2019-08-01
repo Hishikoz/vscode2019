861ee7695b5a6fe2574454a4fc318c0fb893a211
huangkai <superkboss@live.cn>
2019-08-01 15:13:02 +0800

    010 create

---
+++ python/010 file.py
@@ -0,0 +1,41 @@
+import json
+
+def writejson():
+    mydict = {
+        'name': '骆昊',
+        'age': 38,
+        'qq': 957658,
+        'friends': ['王大锤', '白元芳'],
+        'cars': [
+            {'brand': 'BYD', 'max_speed': 180},
+            {'brand': 'Audi', 'max_speed': 280},
+            {'brand': 'Benz', 'max_speed': 320}
+        ]
+    }
+    try:
+        with open('data.json', 'w', encoding='utf-8') as fs:
+            json.dump(mydict, fs)
+    except IOError as e:
+        print(e)
+    print('保存数据完成！')
+
+def readjson():
+    try:
+        with open('data.json', 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+        print(lines)
+    except FileNotFoundError:
+        print('无法打开指定的文件!')
+    except LookupError:
+        print('指定了未知的编码!')
+    except UnicodeDecodeError:
+        print('读取文件时解码错误!')
+
+
+def main():
+    # writejson()
+    readjson()
+
+
+if __name__ == '__main__':
+    main()
